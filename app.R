be64302b95f7227495569fa8dc93e6299326e3ba
Frank <frankzheng20@outlook.com>
2022-06-04 16:38:14 -0700

    testing of page 1

--- app.R
+++ app.R
@@ -1,23 +1,37 @@
 library(shiny)
-source("ipage3.R")
+library(ggplot2)
+library(dplyr)
+usregion <- read.csv("data/usregiongas.csv")
 
   ui <- fluidPage(
     navbarPage("Final Project Introduction",
-               ipage_1,
-               ipage_2,
-               ipage_3,
-               summary
+               ipage_1
     )
   )
 
+  
   server <- function(input, output) {
-    output$price_trend <-renderPlot({
-      ggplot(us_city_avg_df, mapping = aes(x = Year, y = yearly_average)) + 
-        geom_line() +
-        labs(title = "US Yearly City Average Gas Prices",
-             subtitle = "unleaded regular gasoline, per gallon",
-             y = "Yearly Average Gas Price (in USD)")
+    
+    region_data_finder <- function(year) {
+      region_names <- gsub("\\.", " ", colnames(usregion))
+      regiondf <- usregion %>% 
+        filter(Date == year) %>% 
+        select(East.Coast:West.Coast..PADD.5.)
+      region_found <- colnames(regiondf)[max.col(regiondf)]
+      region_found <- gsub("\\.", " ", region_found)
+      region_datadf <- data.frame(region = region_names[-1],
+                                  price = as.numeric(regiondf[1, ]))
+      regionplot <- ggplot(region_datadf, aes(x = region, y = price, fill = region)) + 
+        geom_bar(stat = "identity") + ggtitle(paste(year, "U.S. Region Gas Prices"))
+      region_data <- list(region_found, regionplot)
+      return(region_data)
+    }
+    
+    region_ans <- region_data_finder(reactive(input$year_chosen))
+    output$regionplot <- renderPlot({
+      region_ans[[2]]
     })
   }
-  
+
   shinyApp(ui = ui, server = server)
+  
--- ipage1.R
+++ ipage1.R
@@ -1,5 +1,15 @@
 # Interactive page one
+# Which region had the highest gas price for all grades?
 
-ipage_1 <- tabPanel("Interactive Page 1"
-  
-)
+library(ggplot2)
+usregion <- read.csv("data/usregiongas.csv")
+years <- usregion %>% 
+  select(Date) %>% 
+  pull()
+
+ipage_1 <- tabPanel("Interactive Page 1", 
+                    selectInput(inputId = "year_chosen", label = "What year would you like to chose?",
+                                choices = list("1993" = 1993, "1994" = 1994), selected = 1),
+                    plotOutput("regionplot")
+) 
+
