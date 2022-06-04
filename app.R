6e6e9cb416ab1baafd8474598db372ab262ef4b8
ryanngu023 <ryannguyen10023@gmail.com>
2022-06-03 21:15:43 -0700

    basic testing for interactive page 3

--- app.R
+++ app.R
@@ -1,14 +1,23 @@
 library(shiny)
+source("ipage3.R")
 
   ui <- fluidPage(
     navbarPage("Final Project Introduction",
                ipage_1,
                ipage_2,
                ipage_3,
-               summary,
+               summary
     )
   )
 
   server <- function(input, output) {
-    
+    output$price_trend <-renderPlot({
+      ggplot(us_city_avg_df, mapping = aes(x = Year, y = yearly_average)) + 
+        geom_line() +
+        labs(title = "US Yearly City Average Gas Prices",
+             subtitle = "unleaded regular gasoline, per gallon",
+             y = "Yearly Average Gas Price (in USD)")
+    })
   }
+  
+  shinyApp(ui = ui, server = server)
--- ipage3.R
+++ ipage3.R
@@ -1,4 +1,23 @@
+library(ggplot2)
+library(dplyr)
 
-ipage_3 <- tabPanel("Interactive Page 3"
+us_city_avg_df <- read.csv("Data/usReport.csv", skip = 6)
+
+# average pricing for each year
+us_city_avg_df <- us_city_avg_df %>% 
+  mutate(yearly_average = 
+           rowMeans(select(us_city_avg_df, Jan:Dec), na.rm = TRUE))
+
+# create line chart of average gas pricing over years
+
+ipage_3 <- tabPanel("Interactive Page 3",
+                    sidebarLayout(
+                      sidebarPanel(
+                        h4("Controls")
+                      ),
+                      mainPanel(
+                        plotOutput(outputId = "price_trend")
+                      )
+                    )
   
 )
