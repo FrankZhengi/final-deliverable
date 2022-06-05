fbdd3d658c972d9b50ff6ebefd9af69d93cb5bcc
Frank <frankzheng20@outlook.com>
2022-06-04 18:25:44 -0700

    updated ipage 1

--- app.R
+++ app.R
@@ -30,14 +30,24 @@ usregion <- read.csv("data/usregiongas.csv")
         geom_bar(stat = "identity") + ggtitle(paste(year, "U.S. Region Gas Prices"))
     })
     
+    
+    # returning text of region with highest gas price in chosen year
     output$regionmax <- renderText({
       year <- input$year_chosen
+      region_names <- gsub("\\.", " ", colnames(usregion))
       regiondf <- usregion %>% 
         filter(Date == year) %>% 
         select(East.Coast:West.Coast..PADD.5.)
-      region_found <- colnames(regiondf)[max.col(regiondf)]
-      region_found <- gsub("\\.", " ", region_found)
-      paste("The region with the largest gas price is", region_found)
+      region_datadf <- data.frame(region = region_names[-1],
+                                  price = as.numeric(regiondf[1, ]))
+      region_found <- region_datadf %>% 
+        filter(price == max(price, na.rm = T)) %>% 
+        pull(region)
+      if (year == 1993) {
+        paste("No information available for this year.")
+      } else {
+        paste0("The region with the largest gas price is the " , region_found, ".")
+      }
     })
   }
 
--- ipage1.R
+++ ipage1.R
@@ -7,9 +7,10 @@ years <- usregion %>%
   select(Date) %>% 
   pull()
 
-ipage_1 <- tabPanel("Interactive Page 1", 
+ipage_1 <- tabPanel("Interactive Page 1",
+                    titlePanel("Which region has the highest gas price for all grades in a chosen year?"),
                     selectInput(inputId = "year_chosen", label = "What year would you like to chose?",
-                                choices = years, selected = 1),
+                                    choices = years, selected = 1),
                     plotOutput("regionplot"),
                     textOutput("regionmax")
 ) 
