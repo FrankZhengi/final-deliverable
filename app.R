5441c881f62c0304d1d431b580995d186b14a2ca
Frank <frankzheng20@outlook.com>
2022-06-06 00:22:15 -0700

    merging issues with summary and interactive page 2

--- app.R
+++ app.R
@@ -70,7 +70,7 @@ minimized_df <- us_city_avg_df
     output$ye <- renderPlotly({
       req(input$USyears)
       if (identical(input$USyears, "")) return(NULL)
-      ye <- ggplot(data = filter(usReport, Year %in% input$USyears), mapping = aes(Year, median)) +
+      ye <- ggplot(data = filter(usReport, Year %in% input$USyears), mapping = aes(Year, mean)) +
         geom_line()
       height <- session$clientData$output_ye_height
       width <- session$clientData$output_ye_width
