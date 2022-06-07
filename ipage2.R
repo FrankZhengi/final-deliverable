88f30936bf3ac132fa10aba2cb8632552999b2da
UW <jyb51@uw.edu>
2022-06-06 17:38:25 -0700

    merging issues with page 2

--- ipage2.R
+++ ipage2.R
@@ -7,9 +7,9 @@ library(shiny)
 library(plotly)
 
 usReport <- read.csv("data/usReport.csv", skip = 6) 
-usReport_no_date <- usReport %>% 
-usReport$mean = apply(usReport, 1, mean, na.rm = T)
-
+usReport_no_date <- usReport %>%
+  select(Jan:Dec)
+usReport$mean = apply(usReport_no_date, 1, mean, na.rm = T)
 
 USyear <- unique(usReport$Year)
 
@@ -19,7 +19,7 @@ ipage_2 <- tabPanel("Interative Page 2", fluid = T,
                     p("On this page, you will find the average gas prices of unleaded regular, per gallon/3.785 liters in U.S.
                       city average (not seasonalyl adjusted) to find the gas price averages over a year, within the
                       span of 46 years, and compare how the average prices of -- ie. 1980 -- differed from the average 
-                      gas prices of year 2000. We have a range from the year 1976, to 2022"),
+                      gas prices of year 2000. A range of data from the year 1976 to 2022 to "),
                     selectizeInput(
                       inputId = "USyears",
                       label = NULL,
